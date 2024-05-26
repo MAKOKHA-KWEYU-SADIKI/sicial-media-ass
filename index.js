93f6421f064607c36af3af4af176fb93ddfc7bc6
MAKOKHA-KWEYU-SADIKI <makokha.14460@students.kyu.ac.ke>
2024-05-26 20:10:55 +0300

    social-media analysis

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('dark-mode-toggle');
+    toggle.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
